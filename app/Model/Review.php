93e53da0a17d30f11ebdcbdba5c33fa0362d0049
gouthammallepula <gouthammallepula@gmail.com>
2018-05-11 09:12:01 +0530

    Relationship Between Products & Review

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\product;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+    	return $this->belongsTo(product::class);
+    }
 }
