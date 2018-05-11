f2b856ea35b6ebd5d5f75fafb6661a15b6a9e833
gouthammallepula <gouthammallepula@gmail.com>
2018-05-11 10:02:03 +0530

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,18 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'Total Price' => round((1 - ($this->discount/100)) * $this->price,2),
+            /*
+               discount/100 = .x
+               1 - .x = .y
+               .y * price = total price 
+
+            */
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'no rating yet',
+            'href' => [ 
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
